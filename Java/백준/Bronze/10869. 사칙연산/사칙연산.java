2fbce7792f8114af257db4fd014d2ea3a0448911
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-07-02 22:13:03 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 표준 입력을 위한 Scanner 객체
+        Scanner input = new Scanner(System.in);
+
+        // 공백을 기준으로 차례로 입력 받기
+        int num_a = input.nextInt();
+        int num_b = input.nextInt();
+
+        System.out.println(num_a + num_b); // A+B
+        System.out.println(num_a - num_b); // A-B
+        System.out.println(num_a * num_b); // A*B
+        System.out.println(num_a / num_b); // A/B
+        System.out.println(num_a % num_b); // A%B
+    }
+}
