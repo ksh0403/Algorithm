bbaa1912fbd4ed798aa8b222519509b12d4ecd89
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-07-04 21:09:25 +0900

    [Bronze V] Title: N 찍기, Time: 1068 ms, Memory: 31800 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
