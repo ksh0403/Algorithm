9f1197cd9e8f1debb6a61e14960a539e0a886a00
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-07-08 03:33:38 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 빠른 입출력을 위한 BufferedReader 와 BufferedWriter
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 문자열의 길이 출력
+        bw.write(String.valueOf(br.readLine().length()));
+
+        // Reader 버퍼 닫기
+        br.close();
+
+        // Writer 버퍼 비운 뒤 닫기
+        bw.flush();
+        bw.close();
+    }
+}
