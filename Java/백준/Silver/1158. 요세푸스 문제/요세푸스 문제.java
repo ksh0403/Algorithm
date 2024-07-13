c0d66115a2a1952345caf4190ea9ac7b7e022ad8
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-07-14 02:29:18 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 568 ms, Memory: 294740 KB -BaekjoonHub

--- Java/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ Java/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,47 +1,54 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        // 빠른 입출력을 위한 BufferedReader 와 BufferedWriter
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
- 
-        Queue<Integer> q = new LinkedList<>();
-        
-        // Queue에 1~N까지 값을 offer한다.
-        for (int i = 1; i <= N; i++) {
-            q.offer(i);
+
+        // 문자열 분리를 위한 StringTokenizer
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // N, K
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // N명의 사람을 담는 LinkedList
+        LinkedList<Integer> n_list = new LinkedList<>();
+
+        // 1 ~ N 값을 리스트에 삽입
+        for (int i = 1; i <= n; i++) {
+            n_list.add(i);
         }
- 
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        
-        // Queue의 사이즈가 1일 때까지 반복한다.
-        while(q.size() != 1) {
-            // K - 1번째까지는 처음에 있던 값을 맨 뒤로 보낸다.
-            for (int i = 0; i < K - 1; i++) {
-                q.offer(q.poll());
+
+        bw.write("<");
+        // 사람이 모두 제거될 때까지 반복 (=N번 반복)
+        for (int i = 0; i < n; i++) {
+            // 첫 번째 출력을 제외하고 콤마 찍기
+            if (i != 0) {
+                bw.write(", ");
             }
-            // K번째 값은 poll한 후 출력한다.
-            sb.append(q.poll() + ", ");
+
+            // k - 1 번째 (1번째부터 시작하므로) 사람을 찾아 제거하는 과정
+            for (int j = 0; j < k; j++) {
+                if (j == (k - 1)) {
+                    // k - 1 번째 사람이라면 출력하며 리스트에서 제거
+                    bw.write(String.valueOf(n_list.remove()));
+                } else {
+                    // 아직 k - 1 번째 사람이 아니라면 리스트에서 제거하여 맨 뒤로 보냄
+                    n_list.add(n_list.remove());
+                }
+            }
         }
-        
-        // Queue의 사이즈가 1일 때는 단순히 poll하면 된다.
-        sb.append(q.poll() + ">");
- 
-        bw.write(sb.toString() + "\n");
+        bw.write(">");
+
+        // Reader 버퍼 닫기
+        br.close();
+
+        // Writer 버퍼 비운 뒤 닫기
         bw.flush();
         bw.close();
-        br.close();
     }
 }
