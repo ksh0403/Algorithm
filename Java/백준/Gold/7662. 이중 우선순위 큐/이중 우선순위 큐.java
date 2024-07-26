47373b03b9e51b4494cbba1d47bca667881ebbf0
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-07-27 05:57:43 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2928 ms, Memory: 432368 KB -BaekjoonHub

--- Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -6,32 +6,49 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
+        // 테스트데이터의 수 T
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
             int k = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> que = new TreeMap<>();
 
-            for (int i = 0; i < k; i++) {
+            // Map을 이용해 숫자와 해당 숫자의 개수를 저장
+            // TreeMap : 이진트리를 기반으로 오름차순으로 정렬되는 Map
+            TreeMap<Integer, Integer> q = new TreeMap<>();
+
+            for (int j = 0; j < k; j++) {
+                // 공백을 기준으로 입력문자열을 나눠 배열에 저장
                 String[] input = br.readLine().split(" ");
                 char ch = input[0].charAt(0);
                 int n = Integer.parseInt(input[1]);
 
+                // I, D에 따라 연산 처리
                 if (ch == 'I') {
-                    que.put(n, que.getOrDefault(n, 0) + 1);
+                    // q에 없는 키라면 개수를 0 + 1 로 하여 추가
+                    // q에 있는 키라면 + 1 하여 해당 값을 대체
+                    q.put(n, q.getOrDefault(n, 0) + 1);
                 } else {
-                    if (que.size() == 0)
+                    // q가 비어있는데 D 연산이 나오면 무시
+                    if (q.size() == 0)
                         continue;
 
-                    int num = n == 1 ? que.lastKey() : que.firstKey();
-                    if (que.put(num, que.get(num) - 1) == 1)
-                        que.remove(num);
+                    // q에 원소가 있을 경우
+                    // 개수를 줄일 키인 num : n이 1이면 최댓값[laskKey()], 아니면 최솟값[firstKey()]
+                    int num = n == 1 ? q.lastKey() : q.firstKey();
+
+                    // num의 개수(value)를 1 감소시킨다.
+                    // 만약 1이라면 Map에서 삭제시킴
+                    if (q.put(num, q.get(num) - 1) == 1)
+                        q.remove(num);
                 }
             }
 
-            System.out.println(que.size() == 0 ? "EMPTY" : que.lastKey() + " " + que.firstKey());
+            // q가 비어있다면 EMPTY, 그렇지 않다면 최댓값과 최솟값을 출력한다.
+            System.out.println(q.size() == 0 ? "EMPTY" : q.lastKey() + " " + q.firstKey());
         }
 
+        // Reader 버퍼 닫기
+        br.close();
     }
-
 }
