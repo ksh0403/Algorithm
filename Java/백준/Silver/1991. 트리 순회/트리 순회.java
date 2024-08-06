8c5a6098efad7e5c5f04deaa9d1612bad7844137
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-08-06 23:35:02 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- Java/백준/Silver/1991. 트리 순회/트리 순회.java
+++ Java/백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,82 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Node {
-    char value;
-    Node left;
-    Node right;
-
-    public Node(char value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static Node[] tree;
-
-    // 전위 순회
-    public static void preorder(Node node) {
-        if (node == null) return;
-        System.out.print(node.value);
-        preorder(node.left);
-        preorder(node.right);
-    }
-
-    // 중위 순회
-    public static void inorder(Node node) {
-        if (node == null) return;
-        inorder(node.left);
-        System.out.print(node.value);
-        inorder(node.right);
-    }
-
-    // 후위 순회
-    public static void postorder(Node node) {
-        if (node == null) return;
-        postorder(node.left);
-        postorder(node.right);
-        System.out.print(node.value);
-    }
-
-    public static void main(String[] args) throws IOException {
+    static Node head = new Node('A', null, null); // 트리는 A부터 시작
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        tree = new Node[N + 1]; // 노드 배열 생성
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine()); // 띄어쓰기 기준으로 문자열 분리
-            char parentValue = st.nextToken().charAt(0); // nextToken() 메서드로 토큰을 하나씩 꺼내오면 StringTokenizer객체에는 해당 토큰이 사라진다
-            char leftValue = st.nextToken().charAt(0);
-            char rightValue = st.nextToken().charAt(0);
-            
-            // Java에서 char 데이터 타입은 내부적으로 ASCII 코드를 사용
-            if (tree[parentValue - 'A'] == null) { // 부모 노드가 아직 생성되지 않은 경우. 'A'는 문자 'A'의 ASCII 값
-                tree[parentValue - 'A'] = new Node(parentValue); // 부모 노드를 생성
-            }
-            if (leftValue != '.') { // 왼쪽 자식이 존재할 경우
-                tree[leftValue - 'A'] = new Node(leftValue); // 왼쪽 자식 노드를 생성
-                tree[parentValue - 'A'].left = tree[leftValue - 'A']; // 부모 노드와 연결
-            }
-            if (rightValue != '.') { // 오른쪽 자식이 존재할 경우
-                tree[rightValue - 'A'] = new Node(rightValue); // 오른쪽 자식 노드를 생성
-                tree[parentValue - 'A'].right = tree[rightValue - 'A']; // 부모 노드와 연결
-            }
+            st = new StringTokenizer(br.readLine(), " ");
+            char root = st.nextToken().charAt(0);
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+
+            insertNode(head, root, left, right);
         }
 
-        // 전위 순회
-        preorder(tree[0]);
+        preOrder(head);
         System.out.println();
+        inOrder(head);
+        System.out.println();
+        postOrder(head);
+        System.out.println();
+    }
 
-        // 중위 순회
-        inorder(tree[0]);
-        System.out.println();
 
-        // 후위 순회
-        postorder(tree[0]);
-        System.out.println();
+    static class Node {
+        char value;
+        Node left;
+        Node right;
+
+        Node(char value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void insertNode(Node tmp, char root, char left, char right) {
+        if (tmp.value == root) {
+            tmp.left = (left == '.' ? null : new Node(left, null, null));
+            tmp.right = (right == '.' ? null : new Node(right, null, null));
+        } else {
+            if (tmp.left != null) {
+                insertNode(tmp.left, root, left, right);
+            }
+            if (tmp.right != null) {
+                insertNode(tmp.right, root, left, right);
+            }
+        }
+    }
+
+    private static void preOrder(Node node) {
+        if (node == null) { // 더 이상 값이 없을 때 까지
+            return;
+        }
+        System.out.print(node.value); // 출력
+        preOrder(node.left); // 왼쪽 노드 탐색
+        preOrder(node.right); // 오른쪽 노드 탐색
+    }
+
+    private static void inOrder(Node node) {
+        if (node == null) { // 더 이상 값이 없을 때 까지
+            return;
+        }
+        inOrder(node.left); // 왼쪽 노드 탐색
+        System.out.print(node.value); // 출력
+        inOrder(node.right); // 오른쪽 노드 탐색
+    }
+
+    private static void postOrder(Node node) {
+        if (node == null) { // 더 이상 값이 없을 때 까지
+            return;
+        }
+        postOrder(node.left); // 왼쪽 노드 탐색
+        postOrder(node.right); // 오른쪽 노드 탐색
+        System.out.print(node.value); // 출력
     }
 }
