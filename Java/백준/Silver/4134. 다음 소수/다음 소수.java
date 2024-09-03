f860e7c6825f768a8064a7707fd83b9f48641979
SeungYeon <88024939+ksh0403@users.noreply.github.com>
2024-09-04 03:23:47 +0900

    [Silver IV] Title: 다음 소수, Time: 184 ms, Memory: 14388 KB -BaekjoonHub

--- Java/백준/Silver/4134. 다음 소수/다음 소수.java
+++ Java/백준/Silver/4134. 다음 소수/다음 소수.java
@@ -10,38 +10,35 @@ public class Main {
 
         // t만큼 반복
         for (long i = 0; i < t; i++) {
-            long n = Long.parseLong(br.readLine());
-            long x = n;
-
-            while (true) {
-                // 소수이면 sb에 담고 while문 탈출
-                if (isPrime(x)) {
-                    sb.append(x).append("\n");
-                    break;
-                }
+            long x = Long.parseLong(br.readLine());
+            while(!isPrime(x)) {
                 x++;
             }
+            sb.append(x).append('\n');
         }
         System.out.print(sb);
-        br.close();
     }
 
     // 소수 판별 함수
-    public static boolean isPrime(long x) {
+    static boolean isPrime(long num) {
         // 0과 1은 소수가 아님
-        if (x == 0 || x == 1) {
-            return false;
+        if (num < 2) return false;
+
+        // 2와 3은 소수임
+        if (num == 2 || num == 3) return true;
+
+        // 2 또는 3으로 나누어 떨어지면 소수가 아님
+        if (num % 2 == 0 || num % 3 == 0) return false;
+
+        // 5부터는 6씩 더하며 소수 판별
+        // 5부터는 소수가 [6k +- 1] 형태이기 때문
+        for (long i = 5; i <= Math.sqrt(num); i += 6) {
+            // i 로 나눠 6k - 1 값을 판별하고
+            // i+2 로 나눠 6k + 1 값을 판별함
+            if (num % i == 0 || num % (i + 2) == 0) return false;
         }
 
-        // Math.sqrt() : 제곱근 함수
-        for (int j = 2; j <= Math.sqrt(x); j++) {
-            // 약수가 존재하면 소수가 아님
-            if (x % j == 0) {
-                return false;
-            }
-        }
-
-        // 약수가 존재하지 않으면 소수
+        // 소수일 경우 true 반환
         return true;
     }
 }
