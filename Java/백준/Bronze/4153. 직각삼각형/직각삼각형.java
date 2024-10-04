e489348487ce5b53e1dcde1be0f6513050e84be2
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-04 17:22:22 +0900

    [Bronze III] Title: 직각삼각형, Time: 96 ms, Memory: 14044 KB -BaekjoonHub

--- Java/백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ Java/백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] edge = new int[3];
         for (int i = 0; i < 3; i++) {
@@ -15,8 +16,8 @@ public class Main {
 
         while (edge[0] != 0 && edge[1] != 0 && edge[2] != 0) {
             Arrays.sort(edge);
-            if ((edge[0]*edge[0] + edge[1]*edge[1]) == edge[2]*edge[2]) System.out.println("right");
-            else System.out.println("wrong");
+            if ((edge[0]*edge[0] + edge[1]*edge[1]) == edge[2]*edge[2]) sb.append("right").append("\n");
+            else sb.append("wrong").append("\n");
 
             // 다음 세변 입력받기
             st = new StringTokenizer(br.readLine(), " ");
@@ -24,6 +25,7 @@ public class Main {
                 edge[i] = Integer.parseInt(st.nextToken());
             }
         }
+        System.out.print(sb);
         br.close();
     }
 }
