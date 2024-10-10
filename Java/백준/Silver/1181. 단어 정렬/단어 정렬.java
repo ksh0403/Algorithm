056645ec62a3ffac87c77202c7c54cd4ceafdbe2
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-10 16:01:04 +0900

    [Silver V] Title: 단어 정렬, Time: 976 ms, Memory: 24556 KB -BaekjoonHub

--- Java/백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ Java/백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -5,13 +5,17 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
+    int max_length = 0;
     String[] arr = new String[n];
     for (int i = 0; i < n; i++) {
       arr[i] = br.readLine();
+      if (max_length < arr[i].length()) {
+        max_length = arr[i].length();
+      }
     }
   
     ArrayList<String> list = new ArrayList<>();
-    for (int i = 1; i <= 50; i++) {
+    for (int i = 1; i <= max_length; i++) { // max_length 까지만 반복
       ArrayList<String> mini_list = new ArrayList<>();
       for (int j = 0; j < n; j++) {
         if (arr[j].length() == i) {
@@ -22,6 +26,7 @@ public class Main {
         }
       }
       // 한 길이가 끝날 때마다 mini_list를 정렬하여 list에 합침
+      // ArrayList를 정렬하기 위해 Collections.sort() 사용
       Collections.sort(mini_list);
       for (String ele : mini_list) {
         list.add(ele);
