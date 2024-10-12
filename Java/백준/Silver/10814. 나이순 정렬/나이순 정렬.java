d38734ee2da2808615c4cab08f87dc0c9b307b9a
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-13 02:17:30 +0900

    [Silver V] Title: 나이순 정렬, Time: 368 ms, Memory: 41808 KB -BaekjoonHub

--- Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,39 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    TreeMap<Integer, ArrayList<String>> map = new TreeMap<>();
-    StringTokenizer st;
-    for (int i = 0; i < n; i++) {
-      st = new StringTokenizer(br.readLine(), " ");
-      int age = Integer.parseInt(st.nextToken());
-      String name = st.nextToken();
+        StringBuilder[] p = new StringBuilder[201];
 
-      if (map.containsKey(age)) {
-        // age 값에 해당하는 키가 이미 있으면 name을 value_list에 추가함
-        map.get(age).add(name);
-      } else {
-        // 키가 없으면 value_list를 생성하고 map에 저장
-        ArrayList<String> value_list = new ArrayList<>();
-        value_list.add(name);
+        for (int i = 0; i < p.length; i++) {
+            p[i] = new StringBuilder();
+        }
 
-        map.put(age, value_list); // put
-      }
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+
+            p[age].append(age).append(' ').append(name).append('\n');
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(StringBuilder val : p){
+            sb.append(val);
+        }
+        System.out.println(sb);
     }
-
-    Iterator<Integer> keys = map.keySet().iterator();
-    while (keys.hasNext()) {
-      int key = keys.next();
-      for (int i = 0; i < map.get(key).size(); i++) {
-        sb.append(key + " " + map.get(key).get(i)).append("\n");
-      }
-    }
-    
-    System.out.print(sb);
-    br.close();
-  }
 }
