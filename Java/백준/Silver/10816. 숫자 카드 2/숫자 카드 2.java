4066d4296cad74b52fdb7d0c3adba30281ee7103
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-19 21:55:26 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 748 ms, Memory: 195956 KB -BaekjoonHub

--- Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,30 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] cards = new int[20000001]; // 배열의 모든 요소는 자동으로 0으로 초기화됨
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            // map에 없는 키라면 개수를 0 + 1 로 하여 추가
-            // map에 있는 키라면 + 1 하여 해당 값을 대체
-            map.put(x, map.getOrDefault(x, 0) + 1);
+            // 숫자를 입력받으면 해당 숫자에 +10000000을 더해서 그 인덱스의 값을 1 증가
+            cards[Integer.parseInt(st.nextToken()) + 10000000]++;
         }
 
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int j = 0; j < m; j++) {
-            int num = Integer.parseInt(st.nextToken());
-            
-            if (map.containsKey(num)) {
-                sb.append(map.get(num) + " ");
-            } else {
-                sb.append(0 + " ");
-            }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            sb.append(cards[Integer.parseInt(st.nextToken()) + 10000000]).append(' ');
         }
         System.out.println(sb);
         br.close();
