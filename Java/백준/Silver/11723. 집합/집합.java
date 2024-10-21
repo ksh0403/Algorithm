f43b5ec9ed14f79eed2d6a24af697d8a917bef2f
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-22 01:51:27 +0900

    [Silver V] Title: 집합, Time: 1140 ms, Memory: 312932 KB -BaekjoonHub

---
+++ Java/백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        LinkedList<Integer> s = new LinkedList<>(); // 공집합 S
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String order = st.nextToken();
+
+            // 연산 수행
+            if (order.equals("all")) {
+                s.clear();
+                for (int j = 1; j <= 20; j++) {
+                    s.add(j);
+                }
+            } else if (order.equals("empty")) {
+                s.clear();
+            } else {
+                int x = Integer.parseInt(st.nextToken());
+                switch (order) {
+                    case "add":
+                        if (!s.contains(x)) {
+                            s.add(x);
+                        }
+                        break;
+                    case "remove":
+                        if (s.contains(x)) {
+                            s.remove(s.indexOf(x));
+                        }
+                        break;
+                    case "check":
+                        if (s.contains(x)) {
+                            sb.append(1 + "\n");
+                        } else {
+                            sb.append(0 + "\n");
+                        }
+                        break;
+                    case "toggle":
+                        if (s.contains(x)) {
+                            s.remove(s.indexOf(x));
+                        } else {
+                            s.add(x);
+                        }
+                        break;
+                }
+            }
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
