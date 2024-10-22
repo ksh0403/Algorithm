65e23b85ee53fb2d66e14de23ec167b8f137734a
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-23 05:02:19 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 26072 KB -BaekjoonHub

---
+++ Java/백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        LinkedList<String> result = new LinkedList<>();
+        for (int j = 0; j < m; j++) {
+            String input = br.readLine();
+            if (set.contains(input)) {
+                result.add(input);
+            }
+        }
+        Collections.sort(result);
+        sb.append(result.size()).append("\n");
+        for (String ele : result) {
+            sb.append(ele).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
