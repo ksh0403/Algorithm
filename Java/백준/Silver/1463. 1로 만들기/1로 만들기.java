ea07d23e38fccd8fd771683d7efa82ea3b1c3f71
SeungYo <88024939+ksh0403@users.noreply.github.com>
2024-10-26 04:16:03 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

--- Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,36 +1,18 @@
 import java.io.*;
 
 public class Main {
-    static Integer[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        dp = new Integer[n + 1];
-        dp[0] = dp[1] = 0;
-
-        System.out.print(recur(n));
+        
+        System.out.println(recur(n, 0));
     }
 
-    static int recur(int n) {
-        if (dp[n] == null) {
-            // 6으로 나눠지는 경우
-            if (n % 6 == 0) {
-                dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-            }
-            // 3으로만 나눠지는 경우
-            else if (n % 3 == 0) {
-                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-            }
-            // 2로만 나눠지는 경우
-            else if (n % 2 == 0) {
-                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-            }
-            // 2와 3으로 나누어지지 않는 경우
-            else {
-                dp[n] = recur(n - 1) + 1;
-            }
+    static int recur(int n, int count) {
+        // n이 2 미만인 경우 누적된 count값을 반환
+        if (n < 2) {
+            return count;
         }
-        return dp[n];
+        return Math.min(recur(n / 2, count + 1 + (n % 2)), recur(n / 3, count + 1 + (n % 3)));
     }
 }
