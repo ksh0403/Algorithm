23cefc14c086c75c5975ee45df10746e6a07e8e5
SeungYo <88024939+ksh0403@users.noreply.github.com>
2025-04-24 01:59:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 295.59 ms, Memory: 96.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,15 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        PriorityQueue<String> par = new PriorityQueue<>();
-        for (int i = 0; i < participant.length; i++) {
-            par.add(participant[i]);
-        }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        PriorityQueue<String> com = new PriorityQueue<>();
         for (int i = 0; i < completion.length; i++) {
-            com.add(completion[i]);
-        }
-        
-        // 비교
-        for (int i = 0; i < completion.length; i++) {
-            String com_str = com.poll();
-            String par_str = par.poll();
-            if (!com_str.equals(par_str)) {
-                return par_str;
+            if (!completion[i].equals(participant[i])) {
+                return participant[i];
             }
         }
-        return par.poll();
+        return participant[participant.length-1];
     }
 }
