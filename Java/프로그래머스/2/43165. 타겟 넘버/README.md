9ed777ee434b5ac7cc40e6c93b1d3a66f71dbe49
SeungYo <88024939+ksh0403@users.noreply.github.com>
2025-04-23 17:03:44 +0900

    [level 2] Title: 타겟 넘버, Time: 4.76 ms, Memory: 81.5 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/README.md
+++ Java/프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 170 MB, 시간: 226.73 ms
+메모리: 81.5 MB, 시간: 4.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 20:20:49
+2025년 04월 23일 17:03:40
 
 ### 문제 설명
 
--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,34 +1,23 @@
-import java.util.*;
-
 class Solution {
+    int answer;
+    
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
-        // 큐 생성: 큐의 각 원소는 [현재까지의 합, 현재 인덱스]
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0}); // 생성과 동시에 [0, 0]로 초기화한 배열을 큐에 넣음 (탐색을 시작하기 위한 출발점: 합 0, 인덱스 0)
-        
-        // 큐가 빌 때까지 반복 (BFS 탐색)
-        while (!q.isEmpty()) {
-            int[] curr = q.poll(); // 큐에서 하나 꺼냄
-            int sum = curr[0]; // 현재까지의 누적 합
-            int idx = curr[1]; // 현재 처리 중인 인덱스
-            
-            // 종료 조건: 모든 숫자를 다 사용했을 때
-            if (idx == numbers.length) {
-                // 타겟 넘버가 만들어졌다면 카운트
-                if (sum == target) {
-                    answer++;
-                }
-                
-                continue; // 다음 탐색으로 이동
+        answer = 0;
+        dfs(numbers, target, 0, 0);        
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int target, int idx, int sum) {
+        // 종료조건: numbers의 숫자를 모두 사용했을 때
+        if (idx == numbers.length) {
+            // 숫자를 조합한 총 합이 타겟 넘버이면 정답 카운트
+            if (sum == target) {
+                answer++;
             }
-            
-            // 다음 숫자를 더하거나 빼는 두 가지 경우 큐에 넣기
-            q.offer(new int[]{sum + numbers[idx], idx + 1});
-            q.offer(new int[]{sum - numbers[idx], idx + 1});
+            return;
         }
         
-        return answer;    
+        dfs(numbers, target, idx+1, sum + numbers[idx]);
+        dfs(numbers, target, idx+1, sum - numbers[idx]);
     }
 }
