dd8d5d60c73a7d9c8f56cb87600f26950c0f4f91
SeungYo <88024939+ksh0403@users.noreply.github.com>
2025-04-23 17:39:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.91 ms, Memory: 54.2 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,43 +3,41 @@ import java.util.*;
 class Solution {
     int answer = 0;
     boolean[][] visited; // 방문 여부 체크 배열
-    int[][] result; // 거리 누적 저장을 위한 배열
+    int[][] result; // 각 칸별 최소 거리 저장 배열
     
     // 상하좌우 방향 이동을 위한 배열
     int[] dx = { -1, 1, 0, 0 };
     int[] dy = { 0, 0, -1, 1 };
     
     // maps의 크기
-    int n; // 행의 개수
-    int m; // 열의 개수
+    int n;
+    int m;
     
     public int solution(int[][] maps) {
-        n = maps.length; // maps의 원소 개수 !!! (즉, 행의 개수)
-        m = maps[0].length; // maps의 첫 번째 행의 원소 개수 !!! (즉, 열의 개수)
+        n = maps.length; // maps의 행의 개수
+        m = maps[0].length; // maps의 열의 개수
         visited = new boolean[n][m]; // 방문 여부 체크 배열 초기화
-        result = new int[n][m]; // 거리 누적 저장 배열 초기화
+        result = new int[n][m]; // 각 칸별 최소 거리 저장 배열 초기화
         
-        // BFS 시작 (0, 0 위치(라고 생각하고)부터 시작)
+        // BFS 시작 (0, 0 위치부터라고 생각하고 시작)        
         bfs(0, 0, maps);
-     
-        // 탐색 이후 (n-1, m-1) 위치의 방문 여부 확인
-        // 방문했다면 정답을 출력하고, 방문하지 않았다면 -1 출력
+        
+        // 탐색 이후 (n-1, m-1) 위치를 방문했다면 정답 출력, 그렇지 않다면 -1 출력
         if (visited[n-1][m-1]) {
             return result[n-1][m-1] + 1; // 시작 칸 포함이라 +1
         } else {
             return -1;
         }
-
     }
     
     // BFS 함수
     public void bfs(int x, int y, int[][] maps) {
-        Queue<int[]> q = new LinkedList<>(); // 탐색을 위한 큐 생성(좌표 x, y 형태로 저장)
+        Queue<int[]> q = new LinkedList<>(); // 탐색을 위한 큐 생성 (좌표 x, y 형태로 저장)
         q.offer(new int[] {x, y}); // 시작점 큐에 추가
         visited[x][y] = true; // 시작점 방문 처리
         
         // 큐가 빌 때까지 반복
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             // 현재 위치 꺼내기
             int[] curr = q.poll();
             int curr_X = curr[0];
@@ -61,12 +59,12 @@ class Solution {
                 }
                 
                 // 3. 이동 가능한 경우
-                q.offer(new int[]{next_X, next_Y}); // 큐에 추가
+                q.offer(new int[] {next_X, next_Y}); // 큐에 추가
                 visited[next_X][next_Y] = true; // 방문 처리
-                result[next_X][next_Y] = result[curr_X][curr_Y] + 1; // 거리 누적 저장
+                result[next_X][next_Y] = result[curr_X][curr_Y] + 1; // 이동한 거리 저장 
             }
+        
         }
-    
+        
     }
-    
 }
